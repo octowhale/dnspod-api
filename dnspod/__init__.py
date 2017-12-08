e1fc3ad9b9f804bd8784d59f7befbbc64aa291db
octowhale <me@octowhale.com>
2017-12-08 21:42:25 +0800

    增加 class DnspodApiException

--- dnspod/__init__.py
+++ dnspod/__init__.py
@@ -1,19 +1,23 @@
 import json
-import requests
+# import requests
+from requests import session
 
 
 class DnspodApi(object):
-    def __init__(self, user_token=None):
+    def __init__(self, user_token=None, timeout=3):
         self.domain = 'api.dnspod.com'
         self.base_url = 'https://' + self.domain
         self.user_token = user_token
 
-        self.s = requests.session()
+        # self.s = requests.session()
+        self.s = session()
         self.s.headers.update({"Content-type": "application/x-www-form-urlencoded",
                                "Accept": "text/json",
                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"})
+        self.s.timeout = timeout
 
     def login(self, email, password):
+        """login to get user token"""
 
         uri = '/Auth'
         params = dict(login_email=email,
@@ -28,9 +32,10 @@ class DnspodApi(object):
         self.user_token = resp_json.get('user_token')
 
     def do_request(self, method, **kw):
+        """request dnspod api"""
 
         if self.user_token is None:
-            raise "No user_token, login first"
+            raise DnspodApiException('No user_token, login first')
 
         params = dict(user_token=self.user_token,
                       format='json')
@@ -39,10 +44,20 @@ class DnspodApi(object):
         url = '{baseurl}/{method}'.format(baseurl=self.base_url, method=method)
         r = self.s.post(url,
                         data=params)
+
+        # print(r.text)
         resp_json = json.loads(r.text)
 
         if resp_json.get('status').get('code') != "1":
-            raise resp_json.get('status').get('message')
+            msg = u"Error code {code}: {message}".format(
+                code=resp_json['status']['code'],
+                message=resp_json['status']['message']
+            )
+            raise DnspodApiException(msg, resp_json['status']['code'])
 
         print("*" * 10)
         print(json.dumps(resp_json, indent=4, sort_keys=True))
+
+
+class DnspodApiException(Exception):
+    pass
