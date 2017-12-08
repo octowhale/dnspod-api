95bbfe58d35c6c99e52593d150a9aa3641aee59f
octowhale <me@octowhale.com>
2017-12-08 18:09:37 +0800

    增加 class 的 super() 说明

--- dnspod/ApiGlobal.py
+++ dnspod/ApiGlobal.py
@@ -5,6 +5,9 @@
 import requests
 import json
 import re
+import sys
+
+from user import *
 
 
 class Api(object):
@@ -13,8 +16,8 @@ class Api(object):
         self.api_domain = "api.dnspod.com"
         self.base_url = "https://api.dnspod.com"
 
-        self.email = None
-        self.password = None
+        self.email = email
+        self.password = password
         self.user_token = None
         self.s = requests.session()
         self.s.headers.update({"Content-type": "application/x-www-form-urlencoded",
@@ -47,12 +50,12 @@ class Api(object):
         """do api request"""
         if self.user_token is None:
             self.login()
-            print("user_token -> ", self.user_token)
+            # print("user_token -> ", self.user_token)
 
         name = re.sub(r'([A-Z])', r'.\1', uri)
         url = self.base_url + '/' + name[1:]
 
-        print("url -> ", url)
+        # print("url -> ", url)
         params = dict(
             user_token=self.user_token,
             format='json'
@@ -63,6 +66,7 @@ class Api(object):
         r = self.s.post(url, data=params)
 
         resp_json = json.loads(r.text)
+        # print(resp_json)
 
         if resp_json.get('status').get('code') != "1":
             print(resp_json.get('status').get('message'))
@@ -71,19 +75,63 @@ class Api(object):
         return json.loads(r.text)
 
 
+class InfoVersion(Api):
+    """
+    super(): http://python3-cookbook.readthedocs.io/zh_CN/latest/c08/p07_calling_method_on_parent_class.html#id3
+    """
+
+    def __init__(self, *args, **kw):
+        super().__init__()
+
+        # print(self.__class__.__name__)
+        resp_json = super().request(self.__class__.__name__)
+
+        if resp_json:
+            print(resp_json.get('status').get('message'))
+
+
+class Account(Api):
+    """
+    获取当前位置的函数名与行号：
+        http://blog.csdn.net/shanliangliuxing/article/details/7717407
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def UserDetail(self):
+        # print(sys._getframe().f_code.co_name)
+
+        resp_json = super().request(sys._getframe().f_code.co_name)
+
+        if resp_json:
+            print(json.dumps(resp_json.get('info'), indent=4, sort_keys=True))
+
+
 if __name__ == "__main__":
 
-    from user import *
-    client = Api()
-    client.email = email
-    client.password = password
+    client = Account()
+    client.UserDetail()
+
+    # **************** #
+
+    # from user import *
+    # client = Api()
+    # client.email = email
+    # client.password = password
 
     # data = client.login()
     # print(data)
     # client.version()
     # client.user_detail()
 
-    client.InfoVersion()
+    # client.InfoVersion()
 
-    data = client.request('InfoVersion')
-    print(data)
+    # data = client.request('InfoVersion')
+    # print(data)
+
+    # **************** #
+
+    # client2 = InfoVersion()
+    # client2.email = email
+    # client2.password = password
